f539b686da408573321a9b2c4be9607fade232c3
Tlebedeva1989 <tlebedeva1989@gmail.com>
2022-12-01 20:16:58 +0300

    add programm file

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+string[] ChooseStringLess3(string[] array)
+{
+    int count = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3) count++;
+
+    string[] newArray = new string[count];
+    count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+
+    return newArray;
+}
+
+void ShowStringArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+
+string[] Myarray = new string[] {"Hi", "Hello", "World", "Go", "Win"};
+
+ShowStringArray(Myarray);
+string[] cutArray = ChooseStringLess3(Myarray);
+ShowStringArray(cutArray);
+
+
+
+
+ 
